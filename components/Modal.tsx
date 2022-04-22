73f569ce6bc56e814a3310b03d4bde56cea54c18
VaquarShaikh <vaquarshaikh253@gmail.com>
2022-04-23 02:57:14 +0530

    Recitified Modal tsx by installing react types

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -66,13 +66,15 @@ function Modal() {
             </button>
 
             <div className='relative pt-[56.25%] lg:pt-[50%]'>
+            
               <ReactPlayer
                 url={`https://www.youtube.com/watch?v=${trailer}`}
                 width="100%"
                 height="100%"
                 style={{ position: 'absolute', top: '0', left: '0' }}
                 playing
-              muted={muted}
+                muted={muted}
+                
             />
             <div className='absolute bottom-10 flex w-full items-center justify-between px-10'>
               <div className='flex space-x-2'>
